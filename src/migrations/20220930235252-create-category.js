74ff9b48ced913bc7f58b95233fe807f4ee3d79a
Ian Santos <contato.iansantos@gmail.com>
2022-09-30 20:40:44 -0400

    feat: criação da migration de categories

---
+++ src/migrations/20220930235252-create-category.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
