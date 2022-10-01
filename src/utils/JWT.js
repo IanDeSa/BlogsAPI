4d2f4dd64f048ee0607a55120a87ca493c47afb6
Ian Santos <contato.iansantos@gmail.com>
2022-10-01 02:29:46 -0400

    feat: criação de validação de token e tratamento de exceção

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -1,11 +1,27 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-
-const jwtConfig = {
-  expiresIn: '15min',
-  algorithm: 'HS256',
-};
+const jwtConfig = require('./JWT.config');
 
 const tokenGenerator = (payload) => jwt.sign(payload, process.env.JWT_SECRET, jwtConfig);
 
-module.exports = tokenGenerator;
+const tokenValidation = async (token) => {
+  if (!token) {
+    const e = new Error('Missing auth token');
+    e.status = 401;
+    throw e;
+  }
+
+  try {
+    const introspection = await jwt.verify(token, process.env.JWT_SECRET, jwtConfig);
+    return introspection;
+  } catch (e) {
+    const err = new Error('Invalide auth token');
+    err.status = 401;
+    throw err;
+  }
+};
+
+module.exports = {
+  tokenGenerator,
+  tokenValidation,
+};
