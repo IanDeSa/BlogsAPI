7d8160bd2db69e713bda4ac18d7bd42b124f3484
Ian Santos <contato.iansantos@gmail.com>
2022-10-01 01:35:48 -0400

    refactor: mudança na estrutura do model de User, acrescentando mais campos para a chave id e allowNull para as demais

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,14 @@
 const UserSchema = (sequelize, DataTypes) => {
   const UserTable = sequelize.define('User', {
-    id: DataTypes.INTEGER,
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    displayName: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
     image: DataTypes.STRING,
   },
   {
