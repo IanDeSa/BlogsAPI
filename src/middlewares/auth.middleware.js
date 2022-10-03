2ac90e80429fd43875db2375a4a90d7dad450eff
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 03:53:17 -0400

    fix: correção de validação do middleware de atenticação

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,13 +2,7 @@ const { tokenValidation } = require('../utils/JWT');
 
 const authMiddleware = async (req, _res, next) => {
   const token = req.headers.authorization;
-  const payload = await tokenValidation(token);
-
-  if (!payload) {
-    const e = new Error('Error heading jwt token validation');
-    e.status = 401;
-    throw e;
-  }
+  await tokenValidation(token);
 
   next();
 };
