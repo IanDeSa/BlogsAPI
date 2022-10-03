96ec37f1794e946592aa354c174996c49d3210dd
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 04:45:17 -0400

    fix: correção de nome da função

--- src/middlewares/user.middleware.js
+++ src/middlewares/user.middleware.js
@@ -1,7 +1,7 @@
 const userSchema = require('../validations/joi.schemas.js/user.schema');
 
 const user = (req, _res, next) => {
-  const { error } = userSchema.validade(req.body);
+  const { error } = userSchema.validate(req.body);
 
   if (error) {
     const e = new Error(error.message);
