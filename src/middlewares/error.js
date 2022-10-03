3a8dc8c453a8c935e54373b7d82d9427295d9582
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 03:55:11 -0400

    feat: deletando uma pagina

--- src/middlewares/error.js
+++
@@ -1,8 +0,0 @@
-const error = (err, req, res, _next) => {
-  const status = err.status || 500;
-  const message = err.message || 'Erro Interno do Servidor';
-
-  return res.status(status).json({ message });
-};
-
-module.exports = error;
