b0dd0757143cd68468eed39f27699d59c1f190fa
Ian Santos <contato.iansantos@gmail.com>
2022-10-04 03:15:46 -0400

    feat: criação de função getAll no service de post

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,8 +1,18 @@
-// const { PostCategory } = require('../models');
+const { BlogPost, User, Category } = require('../models');
 
-const createPost = async () => {
+const createPost = async () => {};
+
+const getAll = async () => {
+  const posts = await BlogPost.findAll({
+    include: [
+    { model: User, as: 'user', attributes: { exclude: ['password'] } },
+    { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+  return posts;
 };
 
 module.exports = {
   createPost,
+  getAll,
 };
