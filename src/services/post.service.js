0f8262dcfa075dcd8b94fdb19a41fe67fb3929cc
Ian Santos <contato.iansantos@gmail.com>
2022-10-04 03:46:38 -0400

    feat: implementação da função getByPk na camada post service

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -12,7 +12,19 @@ const getAll = async () => {
   return posts;
 };
 
+const getByPk = async (id) => {
+  const post = await BlogPost.findOne({
+    where: { id },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+      ],
+  });
+  return post;
+};
+
 module.exports = {
   createPost,
   getAll,
+  getByPk,
 };
