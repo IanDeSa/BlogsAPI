a77b9938f3dc75e248b98bddb0e09143dc357a82
Ian Santos <contato.iansantos@gmail.com>
2022-10-04 03:45:03 -0400

    criação da rota get post por id

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/', middlewares.authMiddleware, controller.createPost);
 router.get('/', middlewares.authMiddleware, controller.getAll);
+router.get('/:id', middlewares.authMiddleware, controller.getByPk);
 
 module.exports = router;
