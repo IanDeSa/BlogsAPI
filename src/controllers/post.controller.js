50c990f0999a373c087843fe051823616abd7551
Ian Santos <contato.iansantos@gmail.com>
2022-10-04 03:11:32 -0400

    feat: criação da função getAll no post controller

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,6 +5,12 @@ const createPost = async (req, res) => {
   return res.status(201).json({ post });
 };
 
+const getAll = async (_req, res) => {
+  const posts = await service.getAll();
+  return res.status(200).json(posts);
+};
+
 module.exports = {
   createPost,
+  getAll,
 };
