5e6473d42b623575b5eae9a0e01a977eef857311
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 21:49:29 -0400

    feat: inclusão de category e post no index das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const authRoute = require('./auth.router');
 const userRoute = require('./user.router');
+const categoryRoute = require('./category.router');
+const postRoute = require('./post.router');
 
 module.exports = {
   authRoute,
   userRoute,
+  categoryRoute,
+  postRoute,
 };
