cfc684ea1cac867d3a6723d8d934da11bc38a494
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 21:50:25 -0400

    feat: inclusão de category e post no index de services

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,11 @@
 const auth = require('./auth.service');
 const user = require('./user.service');
+const category = require('./category.service');
+const post = require('./post.service');
 
 module.exports = {
   auth,
   user,
+  category,
+  post,
 };
