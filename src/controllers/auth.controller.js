8632fd2b3f4924ea0fa8fec46164b027d43d3dfc
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 21:33:54 -0400

    fix: correção de retorno do controler de autenticação

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const service = require('../services/auth.service');
 
 const auth = async (req, res) => {
   const token = await service.auth(req.body);
-  res.status(200).json({ token });
+  return res.status(200).json({ token });
 };
 
 module.exports = {
