c408ec9905b25dc8929d6e2c4a7222ebe14d8e79
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 21:40:26 -0400

    feat: criação da rota de pegar um usuário por id

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -4,7 +4,13 @@ const controller = require('../controllers/user.controller');
 
 const router = express.Router();
 
-router.post('/', middlewares.userMiddleware, middlewares.authMiddleware, controller.addUser);
 router.get('/', middlewares.authMiddleware, controller.getAll);
+router.get('/:id', middlewares.authMiddleware, async (req, res) => {
+  const { id } = req.params;
+  const user = await controller.getByPk(id);
+  return ((user === null) 
+  ? res.status(404).json({ message: 'User does not exist' }) : res.status(200).json(user));
+});
+router.post('/', middlewares.userMiddleware, controller.addUser);
 
 module.exports = router;
