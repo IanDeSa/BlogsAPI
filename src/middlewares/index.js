31fe8ddf91385ead621cbed2e9a8d492f8539bcd
Ian Santos <contato.iansantos@gmail.com>
2022-10-03 21:48:40 -0400

    feat: inclusão de categoryMiddleware no index

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const authMiddleware = require('./auth.middleware');
 const errorMiddleware = require('./error.middleware');
 const userMiddleware = require('./user.middleware');
+const categoryMiddleware = require('./category.middleware');
 
 module.exports = {
   authMiddleware,
   errorMiddleware,
   userMiddleware,
+  categoryMiddleware,
 };
