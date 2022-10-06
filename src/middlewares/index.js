ad87f8e22929f974cc9f84cf3250822401e66c26
Ian Santos <contato.iansantos@gmail.com>
2022-10-06 15:27:31 -0400

    feat: adição de post middleware nos index de middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,10 +2,12 @@ const authMiddleware = require('./auth.middleware');
 const errorMiddleware = require('./error.middleware');
 const userMiddleware = require('./user.middleware');
 const categoryMiddleware = require('./category.middleware');
+const postMiddleware = require('./post.middleware');
 
 module.exports = {
   authMiddleware,
   errorMiddleware,
   userMiddleware,
   categoryMiddleware,
+  postMiddleware,
 };
