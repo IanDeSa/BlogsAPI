e1f7ab5ae5d79617f7bebd7ca8166a88a11ebb65
Ian Santos <contato.iansantos@gmail.com>
2022-10-06 15:16:46 -0400

    criação da rota getByPk de category

--- src/routes/category.router.js
+++ src/routes/category.router.js
@@ -5,6 +5,7 @@ const middlewares = require('../middlewares');
 const router = express.Router();
 
 router.get('/', middlewares.authMiddleware, controller.getAll);
+router.get('/:id', middlewares.authMiddleware, controller.getByPk);
 router
 .post('/', middlewares.categoryMiddleware, middlewares.authMiddleware, controller.addCategory);
 
