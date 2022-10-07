a8c0606d0a5ca5ef6f77920847792d35a10f5411
Ian Santos <contato.iansantos@gmail.com>
2022-10-06 22:44:17 -0400

    feat: criação do post middleware de validação do esquema updated

--- src/middlewares/post.middleware.js
+++ src/middlewares/post.middleware.js
@@ -1,7 +1,7 @@
 const postSchema = require('../validations/joi.schemas.js/post.schema');
 
 const user = (req, _res, next) => {
-  const { error } = postSchema.validate(req.body);
+  const { error } = postSchema.postSchema.validate(req.body);
 
   if (error) {
     const e = new Error('Some required fields are missing');
@@ -12,4 +12,19 @@ const user = (req, _res, next) => {
   next();
 };
 
-module.exports = user;
+const updated = (req, _res, next) => {
+  const { error } = postSchema.UpdatedSchema.validate(req.body);
+
+  if (error) {
+    const e = new Error('Some required fields are missing');
+    e.status = 400;
+    throw e;
+  }
+
+  next();
+};
+
+module.exports = {
+  user,
+  updated,
+};
