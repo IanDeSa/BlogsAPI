bed10011dd56507f3e39bae7e7ec1ae399840668
Ian Santos <contato.iansantos@gmail.com>
2022-10-06 22:42:34 -0400

    feat: criação rota put /:id com middlewares de autenticação

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -4,8 +4,10 @@ const middlewares = require('../middlewares');
 
 const router = express.Router();
 
-router.post('/', middlewares.postMiddleware, middlewares.authMiddleware, controller.addPost);
+router.post('/', middlewares.postMiddleware.user, middlewares.authMiddleware, controller.addPost);
 router.get('/', middlewares.authMiddleware, controller.getAll);
 router.get('/:id', middlewares.authMiddleware, controller.getByPk);
+router
+.put('/:id', middlewares.authMiddleware, middlewares.postMiddleware.updated, controller.updated);
 
 module.exports = router;
